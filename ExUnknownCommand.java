47ef92e406c7ad40f5b653fdba0d5d5177675843
Shreyan Dutta <luftwaffeswas123@gmail.com>
2020-02-09 09:59:40 +0530

    Create ExUnknownCommand.java

---
+++ ExUnknownCommand.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+public class ExUnknownCommand extends Exception
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public ExUnknownCommand()
+	{
+		super("Unknown command");
+	}
+	public ExUnknownCommand(String message)
+	{
+		super(message);
+	}
+}
