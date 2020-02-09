16b0bb26926c53d0ce9491b515f82b3a872c0331
Shreyan Dutta <luftwaffeswas123@gmail.com>
2020-02-09 09:53:38 +0530

    Create ExInsufficientArguments.java

---
+++ ExInsufficientArguments.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ExInsufficientArguments extends Exception
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public ExInsufficientArguments()
+	{
+		super("Insufficient command arguments!");
+	}
+	public ExInsufficientArguments(String message)
+	{
+		super(message);
+	}
+}
