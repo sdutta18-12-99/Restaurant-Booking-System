b53350fef01c2e4a52e9bd5e576303f0bfce112d
Shreyan Dutta <luftwaffeswas123@gmail.com>
2020-02-09 09:52:46 +0530

    Create ExDateHasAlreadyPassed.java

---
+++ ExDateHasAlreadyPassed.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ExDateHasAlreadyPassed extends Exception
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public ExDateHasAlreadyPassed()
+	{
+		super("Date has already passed!");
+	}
+	public ExDateHasAlreadyPassed(String message)
+	{
+		super(message);
+	}
+}
