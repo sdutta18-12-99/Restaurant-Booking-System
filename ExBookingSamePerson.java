81c54af9520cd116d7cad26b1b4ad52957185962
Shreyan Dutta <luftwaffeswas123@gmail.com>
2020-02-09 09:52:00 +0530

    Create ExBookingSamePerson.java

---
+++ ExBookingSamePerson.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ExBookingSamePerson extends Exception
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public ExBookingSamePerson()
+	{
+		super("Booking by the same person for the dining date already exists!");
+	}
+	public ExBookingSamePerson(String message)
+	{
+		super(message);
+	}
+}
